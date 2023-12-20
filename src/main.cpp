31e0d5445b606d69fb79b5764ecdcc7f06cfd92f
Nardi <lorenzo.nardin@studenti.unipd.it>
2023-12-20 22:30:59 +0100

    Added throw_dice function in main It returns a number between 1 and max Added include directives in cpp files

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+//this function returns a number between 1 and max
+int throw_dice(int max);
+
 int main (void)
 {
 	std::cout << "Hello, World!\n";
 
 	return 0;
+}
+
+int throw_dice(int max)
+{
+	return (rand() % max) + 1;
 }
