e61cb8e130251e6ff6d59d45175fec70059c56a9
LORENZO NARDIN <ln2066609@adt114.adt.unipd.it>
2023-12-21 14:05:42 +0100

    Modified Cell.h Added a member variable that shows if the cell is bought or not Added two memeber functions that interact with this new variable

--- include/Cell.h
+++ include/Cell.h
@@ -3,13 +3,24 @@
 //these are the cell types available on the board
 enum CellType
 {
-  Void = 0, Economic, Standard, Luxury
+  Start = 0, Void , Economic, Standard, Luxury
 };
 
 class Cell
 {
+public:
+    
+    //set the new owner
+    void buy_property();
+    
+    //returns the owner, zero if not owned
+    int return_owner() {return property_check;}
   
 private:
+  //cell type
   CellType type;
+  //can i buy the property, if zero it's not already bought
+  int property_check = 0;
   
+  //dovro' fare delle variabili per capire quali giocatori sono presenti nella cella quando andro' a stampare
 };
