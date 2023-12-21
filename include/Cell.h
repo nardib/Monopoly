2bb69ed1a039ba9dcc1951888e91936ee76722d3
LORENZO NARDIN <ln2066609@adt114.adt.unipd.it>
2023-12-21 13:16:45 +0100

    added enum CellType in Cell.h

--- include/Cell.h
+++ include/Cell.h
@@ -1,2 +1,15 @@
+//made by: Lorenzo Nardin 206609
+
+//these are the cell types available on the board
+enum CellType
+{
+  Void = 0, Economic, Standard, Luxury
+};
+
 class Cell
-{};
+{
+  
+private:
+  CellType type;
+  
+};
