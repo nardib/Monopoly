b38d3c6ea4d538c2ba4d4049e96d4cc2b3c66b20
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2023-12-27 14:41:30 +0100

    added override of 2 virtual functions in HumanPlayer.h

--- include/HumanPlayer.h
+++ include/HumanPlayer.h
@@ -1,4 +1,4 @@
-//made by : 
+//made by : Manuel Andretto 2074979
 
 #ifndef HUMANPLAYER_H
 #define HUMANPLAYER_H
@@ -6,6 +6,25 @@
 #include "Player.h"
 
 class HumanPlayer : public Player
-{};
+{
+
+public:
+    bool buy_this_property() override {
+        // Implement the logic for buying the property
+        std::cout << "Do you want to buy this property? (yes/no): ";
+        std::string answer;
+        std::cin >> answer;
+        return (answer == "yes");
+    }
+
+    bool upgrade_this_building() override {
+        // Implement the logic for upgrading the building
+        std::cout << "Do you want to upgrade the building in this cell? (yes/no): ";
+        std::string answer;
+        std::cin >> answer;
+        return (answer == "yes");
+    }
+};
+
 
 #endif
