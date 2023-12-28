cf3a39e612acb01cea2907735cb3d69ddebef6de
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2023-12-28 15:57:36 +0100

    copied HumanPlayer.h into HumanPlayer.cpp and added a do-while loop

--- include/HumanPlayer.h
+++ include/HumanPlayer.h
@@ -9,21 +9,9 @@ class HumanPlayer : public Player
 {
 
 public:
-    bool buy_this_property() override {
-        // Implement the logic for buying the property
-        std::cout << "Do you want to buy this property? (yes/no): ";
-        std::string answer;
-        std::cin >> answer;
-        return (answer == "yes");
-    }
+    bool buy_this_property() override;
 
-    bool upgrade_this_building() override {
-        // Implement the logic for upgrading the building
-        std::cout << "Do you want to upgrade the building in this cell? (yes/no): ";
-        std::string answer;
-        std::cin >> answer;
-        return (answer == "yes");
-    }
+    bool upgrade_this_building() override;
 };
 
 
--- src/HumanPlayer.cpp
+++ src/HumanPlayer.cpp
@@ -1,3 +1,31 @@
 //made by : 
 
 #include "HumanPlayer.h"
+
+bool HumanPlayer::buy_this_property() {
+        // Implement the logic for buying the property
+        std::string answer;
+        do{
+            std::cout << "Do you want to buy this property? (yes/no): ";
+            std::cin >> answer;
+
+            std::cin.clear();
+            fflush(stdin);
+
+            return (answer == "yes");
+        }while(answer != "yes" && answer != "no");
+}
+
+ bool HumanPlayer::upgrade_this_building() {
+        // Implement the logic for upgrading the building
+        std::string answer;
+        do{
+            std::cout << "Do you want to upgrade the building in this cell? (yes/no): ";
+            std::cin >> answer;
+
+            std::cin.clear();
+            fflush(stdin);
+        
+            return (answer == "yes");
+        }while(answer != "yes" && answer != "no");
+}
