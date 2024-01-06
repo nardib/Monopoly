e5da8e60142c81da41fb7a4ba480b2fd782b4b5c
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2024-01-06 16:55:35 +0100

    Modified ComputerPlayer.cpp with a better random

--- src/ComputerPlayer.cpp
+++ src/ComputerPlayer.cpp
@@ -2,18 +2,30 @@
 
 #include "ComputerPlayer.h"
 
+int random(int min, int max)
+{
+	//seed the random number generator
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	//create a distribution for this range
+	std::uniform_int_distribution<> distribution(min, max);
+
+	//return a random number between min and max
+	return distribution(gen);
+}
+
 bool ComputerPlayer::buy_intent() {
         // Implement the logic for buying the property, this function will return true with a probability of 25%
-        srand(time(0)); 
-        int randNum = rand() % 4 + 1;
+        int rnd = random(1, 4);
         
-        return (randNum == 1);
+        return (rnd == 1);
 }
 
  bool ComputerPlayer::upgrade_intent() {
         // Implement the logic for upgrading the building, this function will return true with a probability of 25%
-        srand(time(0));
-        int randNum = rand() % 4 + 1;
+        int rnd = random(1, 4);
         
-        return (randNum == 1);
+        return (rnd == 1);
  }
+
