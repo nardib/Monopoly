513d39c5a2016b2debf7132bbe700d60c4677556
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2024-01-09 16:15:45 +0100

    small fix in computerplayer.cpp

--- src/ComputerPlayer.cpp
+++ src/ComputerPlayer.cpp
@@ -2,30 +2,17 @@
 
 #include "ComputerPlayer.h"
 
-/*
-int random(int min, int max)
-{
-	//seed the random number generator
-	std::random_device rd;
-	std::mt19937 gen(rd());
-
-	//create a distribution for this range
-	std::uniform_int_distribution<> distribution(min, max);
-
-	//return a random number between min and max
-	return distribution(gen);
-}*/
-
 bool ComputerPlayer::buy_intent() {
         // Implement the logic for buying the property, this function will return true with a probability of 25%
         int rnd = random(1, 4);
-		if(rnd == 1){
-                p_property.push_back(pos());
-                std::sort(p_property.begin(), p_property.end());
-                return true;
-            }else{
-                return false;
-            }
+		if(rnd == 1 && board->get_value(pos()).return_type() != (CellType::Void) && board->get_value(pos()).return_type() != (CellType::Start)){
+            p_property.push_back(pos());
+            std::sort(p_property.begin(), p_property.end());
+            
+            return true;
+        }
+        
+        return false;
 }
 
  bool ComputerPlayer::upgrade_intent() {
