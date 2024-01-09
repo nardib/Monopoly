802a7f7576f30bc0406c4645c89e8a175c62fc10
Marco <marco.spolverato.1@studenti.unipd.it>
2024-01-09 16:09:00 +0100

    Reverted changes for fatal flaw on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
 //made by : Marco Spolverato 2032569
 
 #include "Game.h"
-#include <Windows.h>
-#include <cstdio>
 
 int main (int argc, char *argv[])
 {
-	// Set console code page to UTF-8 so console known how to interpret string data
-    SetConsoleOutputCP(CP_UTF8);
-
-    // Enable buffering to prevent VS from chopping up UTF-8 byte sequences
-    setvbuf(stdout, nullptr, _IOFBF, 1000);
 	if(argc > 1)
 		std::cout << "elemento da riga di comando: " << argv[1] << std::endl;
 	Board b {8, 10, 6};
